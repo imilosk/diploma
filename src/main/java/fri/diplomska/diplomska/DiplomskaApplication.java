370d6a4d663666cfdfd9eada77d97c9f8d8298a1
Milosh Kostadinovski <milos.kostadinovski97@gmail.com>
2020-08-23 11:31:36 +0200

    Added support for Tomcat.

--- src/main/java/fri/diplomska/diplomska/DiplomskaApplication.java
+++ src/main/java/fri/diplomska/diplomska/DiplomskaApplication.java
@@ -2,9 +2,17 @@ package fri.diplomska.diplomska;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DiplomskaApplication {
+public class DiplomskaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DiplomskaApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiplomskaApplication.class, args);
