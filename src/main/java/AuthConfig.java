195701882b7fc78f01e7949423aaef227547a719
Milosh Kostadinovski <milos.kostadinovski97@gmail.com>
2020-08-27 23:38:59 +0200

    Disable csrf protection.

---
+++ src/main/java/AuthConfig.java
@@ -0,0 +1,26 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class AuthConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${security.enable-csrf}")
+    private boolean csrfEnabled;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        if(!csrfEnabled)
+        {
+            http.csrf().disable();
+        }
+    }
+}
