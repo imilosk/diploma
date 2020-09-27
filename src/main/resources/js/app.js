e18e4295694a758dcacfc9acef9c0cf3a797300e
Milosh Kostadinovski <milos.kostadinovski97@gmail.com>
2020-09-27 20:17:20 +0200

    Added Apex charts JS chart library.

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import vue2Dropzone from 'vue2-dropzone';
 import 'vue2-dropzone/dist/vue2Dropzone.min.css';
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.component('vue-dropzone', vue2Dropzone);
+Vue.component('apexchart', VueApexCharts)
 Vue.use(VueRouter);
 
 window.axios = require('axios');
@@ -21,10 +23,13 @@ const RepositoryPageComponent = resolve => {
 const K8sPageComponent = resolve => {
     require.ensure([], require => resolve(require('./components/k8s/K8sPageComponent.vue')));
 };
+const DashboardPageComponent = resolve => {
+    require.ensure([], require => resolve(require('./components/dashboard/DashboardPageComponent.vue')));
+};
 
 // 2. Define routes
 const routes = [
-    { path: '/dashboard', component: Foo },
+    { path: '/dashboard', component: DashboardPageComponent },
     { path: '/repository', component: RepositoryPageComponent },
     { path: '/k8s', component: K8sPageComponent },
     { path: '/settings', component: Foo }
@@ -37,6 +42,6 @@ const router = new VueRouter({
 
 // 4. Create and mount the root instance.
 const app = new Vue({
-    router
+    router,
 }).$mount('#app');
 // Now the app has started!
---
+++ src/main/resources/js/components/dashboard/DashboardPageComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="--page-dashboard">
+        <p class="page-title">Dashboard</p>
+        <template>
+            <div>
+                <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            options: {
+                chart: {
+                    id: 'vuechart-example'
+                },
+                xaxis: {
+                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+                }
+            },
+            series: [{
+                name: 'series-1',
+                data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }]
+        }
+    }
+}
+</script>
