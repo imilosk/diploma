9fe664e314f9c48c1b1a10bc56ebd86192988ea1
Milosh Kostadinovski <milos.kostadinovski97@gmail.com>
2020-12-10 18:47:02 +0100

    Added docker folder to gitignore, installed zip library and added support for upload zip files for docker images.

--- src/main/java/fri/diplomska/diplomska/controllers/FileUploadController.java
+++ src/main/java/fri/diplomska/diplomska/controllers/FileUploadController.java
@@ -3,12 +3,18 @@ package fri.diplomska.diplomska.controllers;
 import fri.diplomska.diplomska.docker.ImageBuilder;
 import fri.diplomska.diplomska.helpers.Helper;
 import fri.diplomska.diplomska.kubernetes.Deployer;
+import net.lingala.zip4j.ZipFile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @RestController
 public class FileUploadController {
@@ -17,10 +23,25 @@ public class FileUploadController {
     @RequestMapping(value = "/uploadDockerImage", method  = RequestMethod.POST )
     public ResponseEntity<String> index(@RequestParam("file") MultipartFile file) {
         try {
+            // create a random UUID for folder name
+            String folderName = UUID.randomUUID().toString();
+
+            // path to the file
+            String fileFolder = Helper.getProjectPath() + "docker" + File.separator + folderName + File.separator;
+
+            // create a new directory on the server FS
+            Files.createDirectory(Paths.get(fileFolder));
+            // save the zip file to the server FS
+            Path filePathToSave = Paths.get(fileFolder, "image.zip");
+
+            try (OutputStream os = Files.newOutputStream(filePathToSave)) {
+                os.write(file.getBytes());
+            }
+
+            // unzip the files
+            new ZipFile(filePathToSave.toString()).extractAll(fileFolder);
+
             ImageBuilder imageBuilder = new ImageBuilder();
-            String fileFolder = Helper.getProjectPath() + "docker" + File.separator;
-            //String filePathToSave = fileFolder +  File.separator + "Dockerfile";
-            //file.transferTo(new File(filePathToSave));
             String imageId = imageBuilder.build(fileFolder);
             Deployer deployer = new Deployer();
             deployer.deploy(imageId);
