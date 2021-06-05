bfe6eec483ffc2970c394516b0c4a01d257e4e4d
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2021-06-05 16:55:08 +0200

    Adds data table with search filter

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -5,6 +5,7 @@ import 'vue2-dropzone/dist/vue2Dropzone.min.css';
 import VueApexCharts from 'vue-apexcharts';
 import VueToastr from '@deveodk/vue-toastr';
 import '@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css';
+import _ from 'lodash';
 
 Vue.component('vue-dropzone', vue2Dropzone);
 Vue.component('apexchart', VueApexCharts)
--- src/main/resources/js/components/repository/RepositoryPageComponent.vue
+++ src/main/resources/js/components/repository/RepositoryPageComponent.vue
@@ -1,31 +1,169 @@
 <template>
-  <div id="--page-repository">
-    <p class="page-title">Repository</p>
-
-    <router-link to="/uploadContainer">
-      <button class="py-2 px-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200
-     text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none
-      focus:ring-2 focus:ring-offset-2  rounded-lg " type="button">
-        Upload a new container
-      </button>
-    </router-link>
-    <div class="filters-row">
-      <search-component></search-component>
-      <div class="filters">
-        <filter-component></filter-component>
-        <filter-component></filter-component>
-        <filter-component></filter-component>
+  <div class="py-12 align-middle inline-block min-w-full sm:px-6 lg:px-24">
+    <div class="flex justify-between mt-1 mb-10">
+      <div class="search-box">
+        <label class=" block text-sm font-medium text-gray-700">
+          Search
+        </label>
+        <input
+            id="exp"
+            class="focus:ring-indigo-500 focus:border-indigo-500 block shadow-sm sm:text-sm border-gray-300 rounded-md"
+            name="exp"
+            placeholder='Enter name'
+            type="text"
+            v-model="inputText"
+        />
+      </div>
+      <div class="filters flex space-x-4">
+        <dropdown-menu-component/>
+        <dropdown-menu-component/>
+        <dropdown-menu-component/>
       </div>
     </div>
-    <div class="containers-row">
-      <container-card-component></container-card-component>
-      <container-card-component></container-card-component>
-      <container-card-component></container-card-component>
-      <container-card-component></container-card-component>
+
+    <div class="flex flex-col">
+      <div class="-my-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+              <tr>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    scope="col">
+                  Name
+                </th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    scope="col">
+                  Title
+                </th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    scope="col">
+                  Status
+                </th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    scope="col">
+                  Role
+                </th>
+                <th class="relative px-6 py-3"
+                    scope="col">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="person in filteredList"
+                  :key="person.email">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div class="flex-shrink-0 h-10 w-10">
+                      <img :src="person.image"
+                           alt=""
+                           class="h-10 w-10 rounded-full"/>
+                    </div>
+                    <div class="ml-4">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ person.name }}
+                      </div>
+                      <div class="text-sm text-gray-500">
+                        {{ person.email }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="text-sm text-gray-900">{{ person.title }}</div>
+                  <div class="text-sm text-gray-500">{{ person.department }}</div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    Active
+                  </span>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                  {{ person.role }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <a class="text-indigo-600 hover:text-indigo-900"
+                     href="#">Edit</a>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import DropdownMenu from "./DropdownMenuComponent";
+import DropdownMenuComponent from "./DropdownMenuComponent";
+const people = [
+  {
+    name: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Boki Dandalo',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Simo Cortilo',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Milos Cvabalo',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+  {
+    name: 'Dimitar Becki kocijas',
+    title: 'Regional Paradigm Technician',
+    department: 'Optimization',
+    role: 'Admin',
+    email: 'jane.cooper@example.com',
+    image:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+  },
+]
 
+export default {
+  components: {DropdownMenuComponent, DropdownMenu},
+  data() {
+    return {
+      people,
+      inputText: ''
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    filteredList: function () {
+      return this.people.filter(human => {
+        return human.name.toLowerCase().includes(this.inputText.toLowerCase())
+      });
+    }
+  }
+}
 </script>
