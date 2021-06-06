76a3be43ecf4af050a65e3c604345e3edc2c85e5
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2021-06-06 21:31:47 +0200

    Remove unused css import, move svg to component, code refactor

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import 'vue2-dropzone/dist/vue2Dropzone.min.css';
 import VueApexCharts from 'vue-apexcharts';
 import VueToastr from '@deveodk/vue-toastr';
 import '@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css';
