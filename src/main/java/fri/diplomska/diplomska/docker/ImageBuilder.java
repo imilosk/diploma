7de20e1d173b3e3c048ef23ed0820e3adb095478
Milos Kostadinovski <milos.kostadinovski97@gmail.com>
2021-06-12 21:56:44 +0200

    Rewrite image build without arrow function

--- src/main/java/fri/diplomska/diplomska/docker/ImageBuilder.java
+++ src/main/java/fri/diplomska/diplomska/docker/ImageBuilder.java
@@ -2,6 +2,9 @@ package fri.diplomska.diplomska.docker;
 
 import com.spotify.docker.client.DefaultDockerClient;
 import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.ProgressHandler;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.ProgressMessage;
 
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicReference;
@@ -14,12 +17,15 @@ public class ImageBuilder {
 
         final AtomicReference<String> imageIdFromMessage = new AtomicReference<>();
         final String returnedImageId = docker.build(
-            Paths.get(filePath), imageName, message -> {
-                final String imageId = message.buildImageId();
-                if (imageId != null) {
-                    imageIdFromMessage.set(imageId);
-                }
-            });
+                Paths.get(filePath), imageName, new ProgressHandler() {
+                    @Override
+                    public void progress(ProgressMessage progressMessage) throws DockerException {
+                        final String imageId = progressMessage.buildImageId();
+                        if (imageId != null) {
+                            imageIdFromMessage.set(imageId);
+                        }
+                    }
+                });
         return returnedImageId;
     }
 
